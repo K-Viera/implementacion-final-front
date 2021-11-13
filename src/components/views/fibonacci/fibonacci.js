183eab957cea8f506977b595e9ef4fb016a4b7df
Kevin <kenvi.009@gmail.com>
2021-11-13 16:17:10 -0500

    form complete

--- src/components/views/fibonacci/fibonacci.js
+++ src/components/views/fibonacci/fibonacci.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import { Form, Row, Col, Button, Card, Container } from "react-bootstrap";
 
 class Fibonacci extends React.Component {
+  state = {
+    input: "",
+  };
+
   handleChange = (e) => {
-    console.log("change");
+    this.setState({
+      input: e.target.value,
+    });
+  };
+  handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("enviado");
   };
 
   render() {
@@ -13,10 +23,44 @@ class Fibonacci extends React.Component {
           <Card.Header>
             <Card.Title className="text-center">Fibonacci</Card.Title>
           </Card.Header>
-          <Card.Body>hola</Card.Body>
-          <Form>
+          <Card.Body>
+            <Form onSubmit={this.handleSubmit}>
+              <Row>
+                <Form.Control
+                  type="number"
+                  name="input"
+                  placeholder="Ingrese el valor"
+                  onChange={this.handleChange}
+                  value={this.state.input}
+                  min="0"
+                  required
+                  autoFocus
+                ></Form.Control>
+              </Row>
+              <Row>
+                <Button
+                  size="lg"
+                  variant="outline-primary btn-block"
+                  type="submit"
+                  block
+                >
+                  Calcular
+                </Button>
+              </Row>
+            </Form>
+          </Card.Body>
+        </Card>
+        <br />
+        <Card>
+          <Card.Header>
+            <Card.Title className="text-center">Resultados</Card.Title>
+          </Card.Header>
+          <Card.Body>
+            <Row>
+              Resultado : <textarea value={this.state.input} readOnly />
+            </Row>
             <Row></Row>
-          </Form>
+          </Card.Body>
         </Card>
       </Container>
     );
