290f233936f599268185ac2e8bcc501f3cbfc731
Kevin <kenvi.009@gmail.com>
2021-11-13 15:32:13 -0500

    change redirection

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import logo from "./logo.svg";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-import Fibonacci from "./components/Views/Fibonacci/Fibonacci";
-import Factorial from "./components/Views/Factorial/Factorial";
-import APTriangulo from "./components/Views/APTriangulo/APTriangulo";
-import APTCirculo from "./components/Views/APCirculo/APCirculo";
-import APRectangulo from "./components/Views/APRectangulo/APRectangulo";
+import Fibonacci from "./components/views/fibonacci/fibonacci";
+import Factorial from "./components/views/factorial/factorial";
+import APTriangulo from "./components/views/apTriangulo/apTriangulo";
+import APTCirculo from "./components/views/apCirculo/apCirculo";
+import APRectangulo from "./components/views/apRectangulo/apRectangulo";
 import Layout from "./components/Layouts/Layout";
 function App() {
   return (
