e3191047a9e670e9d18d6ea6cc8f739be9cd1554
Kevin <kenvi.009@gmail.com>
2021-11-14 14:50:26 -0500

    product url

--- src/components/global.js
+++ src/components/global.js
@@ -1,4 +1,5 @@
 const global = {};
 // global.url = "http://localhost:5000";
-global.url = "https://qa-final-back.herokuapp.com";
+// global.url = "https://qa-final-back.herokuapp.com";
+global.url = "https://final-imp-back.herokuapp.com";
 module.exports = global;
